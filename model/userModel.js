7ab829d535b9687412e5e39508d0ba0f8fd77658
Steven M Blakely <email@autisticgeek.com>
2018-05-05 13:31:14 -0600

    added name to schema

--- model/userModel.js
+++ model/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+    name:{required:true, type:String},
     email: { required: true, type: String },
     password: {required:true, type:String},
     emailVerified: {default:false, type:Boolean},
