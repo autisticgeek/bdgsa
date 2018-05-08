972edc472ba6be89721883159b4fdfb3661977c2
Steven M Blakely <email@autisticgeek.com>
2018-05-08 09:40:53 -0600

    changes to user model based on the auth schema

--- model/userModel.js
+++ model/userModel.js
@@ -2,13 +2,29 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name:{required:true, type:String},
-    email: { required: true, type: String },
-    password: {required:true, type:String},
-    emailVerified: {default:false, type:Boolean},
-    accountStatus:{default:"new", type:String},
-    token: {type:String}
-})
+    name: {
+        required: true, type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        required: true, type: String
+    },
+
+    accountStatus: {
+        default: "new", type: String
+    },
+    isAdmin: { type: Boolean, default: false }, emailVerified: {
+        default: false, type: Boolean
+    },
+    emailToken: {
+        ype: String
+    }
+}, {timestamps:true})
 
 const UserModel = mongoose.model("user", UserSchema);
 module.exports = UserModel;
