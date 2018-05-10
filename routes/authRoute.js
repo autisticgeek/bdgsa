785e26d6dd2bf3a90d05e6af4d571e009a9c3854
Steven M Blakely <email@autisticgeek.com>
2018-05-10 14:42:04 -0600

    auth working server side

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,33 +1,37 @@
 const express = require("express");
 const authRouter = express.Router();
-const User = require("../model/userModel.js");
+const UserModel = require("../model/userModel.js");
 const jwt = require("jsonwebtoken");
 
 authRouter.post("/signup", (req, res) => {
-    User.findOne({username: req.body.username}, (err, existingUser) => {
-        if (err) return res.status(500).send({success: false, err});
-        if (existingUser !== null) {
-            return res.status(400).send({success: false, err: "That username already exists!"});
-        }
-        const newUser = new User(req.body);
-        newUser.save((err, user) => {
-            if (err) return res.status(500).send({success: false, err});
-            const token = jwt.sign(user.toObject(), process.env.SECRET);
-            return res.status(201).send({success: true, user: user.toObject(), token});
-        });
-    });
+    UserModel.findOne({ username: req.body.email.toLowerCase() })
+        .exec((err, foundUser) => {
+            if (err) return res.status(500).send(err);
+            if (foundUser) {
+                return res.status(400).send({ success: false, err: "User already exists!" });
+            } else {
+                const newUser = new UserModel(req.body);
+                newUser.save((err, user) => {
+                    if (err) return res.status(500).send(err);
+                    const token = jwt.sign(user.toObject(), process.env.SECRET);
+                    res.status(201).send({ success: true, user: user.withoutPassword(), token });
+                })
+            }
+        })
 });
-
 authRouter.post("/login", (req, res) => {
-    User.findOne({username: req.body.username.toLowerCase()}, (err, user) => {
+    UserModel.findOne({ email: req.body.email.toLowerCase() }, (err, user) => {
         if (err) return res.status(500).send(err);
-        if (!user || user.password !== req.body.password) {
-            return res.status(403).send({success: false, message: "Email or password are incorrect"})
-        }
-        const token = jwt.sign(user.toObject(), process.env.SECRET, {expiresIn: "24h"});
-        return res.send({token: token, user: user.toObject(), success: true, message: "Here's your token!"})
+        if (!user) return res.status(403).send({ success: false, message: "Invalid Username" })
+        user.checkPassword(req.body.password, (err, match) => {
+            if (err) return res.status(500).send(err);
+            if (!match) return res.status(401).send({ success: false, message: "Invalid Password" })
+            const token = jwt.sign(user.toObject(), process.env.SECRET, { expiresIn: "2w" });
+            return res.send({ token: token, user: user.withoutPassword(), success: true })
+        })
+
+
     });
 });
-
 
 module.exports = authRouter
