69ff383d8a55e2a9559b75581dce6acbc4ae5a12
Will Powers <powersrevolution@gmail.com>
2018-05-11 16:15:46 -0600

    added sale type to sale model

---
+++ client/src/redux/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import uuid from "uuid";
+
+const initialState = {
+    data: [],
+    user
+}
--- model/saleModel.js
+++ model/saleModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const saleSchema = new Schema({
+    saleType: { required: true, type: String },
     address: { required: true, type: String },
     startTime: { required: true, type: String },
     endTime: { required: true, type: String },
