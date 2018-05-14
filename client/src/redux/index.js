b5df6fb57e586fa92257c542db0785fbea8a4e28
Will Powers <powersrevolution@gmail.com>
2018-05-14 15:46:21 -0600

    added authorized user to redux

--- client/src/redux/index.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import uuid from "uuid";
-
-const initialState = {
-    data: [],
-    user
-}
--- model/saleModel.js
+++ model/saleModel.js
@@ -7,7 +7,10 @@ const saleSchema = new Schema({
     startTime: { required: true, type: String },
     endTime: { required: true, type: String },
     description: { required: true, type: String },
-    imgUrl: { required: false, type: String }
+    imgUrl: { required: false, type: String },
+    sellerId: { required: true, type: mongoose.Schema.Types.ObjectId,
+    ref: "Users"
+    }
 });
 
 const SaleModel = mongoose.model("sale", saleSchema);
