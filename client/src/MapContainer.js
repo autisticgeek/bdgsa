d3deee5677e7d187fb033aaa31f637871a2a973a
Steven M Blakely <email@autisticgeek.com>
2018-05-16 15:26:57 -0600

    map work

--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import GoogleMapReact from 'google-map-react'
+import axios from "axios"
 
 import { connect } from "react-redux";
 
@@ -13,8 +14,9 @@ class Maps extends Component {
         this.onGeolocateError = this.onGeolocateError.bind(this);
         this.onGeolocateSuccess = this.onGeolocateSuccess.bind(this);
         this.geolocate = this.geolocate.bind(this)
+        
     }
-    onGeolocateSuccess(coordinates) {
+    onGeolocateSuccess = (coordinates) =>{
         const { latitude, longitude } = coordinates.coords;
         console.log('Found coordinates: ', latitude, longitude);
         this.setState(() => {
@@ -25,7 +27,7 @@ class Maps extends Component {
         });
     }
 
-    onGeolocateError(error) {
+    onGeolocateError= (error) => {
         console.warn(error.code, error.message);
 
         if (error.code === 1) {
@@ -36,26 +38,47 @@ class Maps extends Component {
             console.log("timeout")
         }
     }
-    geolocate() {
+    geolocate= () => {
         if (window.navigator && window.navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(this.onGeolocateSuccess, this.onGeolocateError);
         }
     }
+   getSales = ()=> {
+        axios.get(`/sales`).then(responce => {
+            console.log(responce.data)
+        })
+    }
+    componentDidMount() {
+        this.geolocate();
+    }
 
 
     render() {
-        console.log(this.state)
+        console.log("props", this.props)
+
+
+        //let sales = [{lat:40.7, lng:-111.887}, {lat:40.5, lng:-111.8}, {lat:-1.1745995, lng:116.7016587}]
+        let salesArr = []
+        if (this.state.lat) {
+            let sales = this.getSales()
+            salesArr = sales.map(sale => <div lat={sale.lat} lng={sale.lng}><i class="fas fa-location-arrow fa-2x orange mirror"></i></div>)
+
+
+        }
+        console.log(sales);
         return <div className='google-map'>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyAp_gcAL9g64umPJUNU10vjP3Y-MHbmmQo" }}
-                    center={{ lat: 40.7, lng: -111.80 }}
-                    zoom={9}>
-                    <div lat={this.state.lat} lng={this.state.lng}>&mdot;</div>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyAp_gcAL9g64umPJUNU10vjP3Y-MHbmmQo" }}
+                center={{ lat: 40.7, lng: -111.80 }}
+                zoom={9}>
+                <div lat={this.state.lat} lng={this.state.lng}><i class="fas fa-map-marker-alt fa-2x orange"></i></div>
+                {salesArr}
 
 
-                </GoogleMapReact><button onClick={this.geolocate}>Find ME</button>
 
-            </div>
+            </GoogleMapReact>
+
+        </div>
     }
 }
 
